cd6efb269aa615ee369b363f19087a4a0c540a46
Gabbi Nguyen <gabbinguyen@gmail.com>
2020-10-09 15:56:57 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
 
+    def initialize
+        super
+        @knowledge = []
+    end
+
+    def knowledge
+        @knowledge
+    end
+
+    def learn(education)
+        @knowledge << education
+    end
 end
